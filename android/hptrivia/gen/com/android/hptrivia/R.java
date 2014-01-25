03ec7c745fca06cbfed05ee2b78af4a1fda7acb6
Nati Tessema <nati.tessema@gmail.com>
2014-01-25 02:48:33 -0800

    Made xml layout for home screen as well.

--- android/hptrivia/gen/com/android/hptrivia/R.java
+++ android/hptrivia/gen/com/android/hptrivia/R.java
@@ -10,6 +10,24 @@ package com.android.hptrivia;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int aqua=0x7f040009;
+        public static final int black=0x7f04000f;
+        public static final int blue=0x7f04000d;
+        public static final int fuchsia=0x7f040002;
+        public static final int gray=0x7f040005;
+        public static final int green=0x7f04000c;
+        public static final int lime=0x7f04000a;
+        public static final int maroon=0x7f040008;
+        public static final int navy=0x7f04000e;
+        public static final int olive=0x7f040006;
+        public static final int purple=0x7f040007;
+        public static final int red=0x7f040003;
+        public static final int silver=0x7f040004;
+        public static final int teal=0x7f04000b;
+        public static final int white=0x7f040000;
+        public static final int yellow=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,34 +35,38 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int answer_height=0x7f040005;
-        public static final int answer_width=0x7f040004;
-        public static final int question_height=0x7f040003;
-        public static final int question_width=0x7f040002;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int answer_height=0x7f050007;
+        public static final int answer_width=0x7f050006;
+        public static final int app_logo_height=0x7f050003;
+        public static final int app_logo_width=0x7f050002;
+        public static final int question_height=0x7f050005;
+        public static final int question_width=0x7f050004;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int button_answer1=0x7f080001;
-        public static final int button_answer2=0x7f080002;
-        public static final int button_answer3=0x7f080003;
-        public static final int button_answer4=0x7f080004;
-        public static final int textView_question=0x7f080000;
+        public static final int action_settings=0x7f090006;
+        public static final int button_answer1=0x7f090005;
+        public static final int button_answer2=0x7f090004;
+        public static final int button_answer3=0x7f090003;
+        public static final int button_answer4=0x7f090002;
+        public static final int textView_app_title=0x7f090000;
+        public static final int textView_question=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_quiz_question=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -68,10 +90,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
