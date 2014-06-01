973da84905b686675f3f3b3721682d8de9793ee8
Nati <nati.tessema@gmail.com>
2014-06-01 14:35:48 -0700

    Adding Question object.

---
+++ hptrivia/app/src/main/java/com/titan/hptrivia/model/Question.java
@@ -0,0 +1,61 @@
+package com.titan.hptrivia.model;
+
+import org.json.JSONObject;
+
+/**
+ * Created by ntessema on 6/1/14.
+ */
+public class Question {
+
+    private static final String TAG = Question.class.getSimpleName();
+
+    private String questionText;
+    private String correctAnswerText;
+    private String wrongAnswer1Text;
+    private String wrongAnswer2Text;
+    private String wrongAnswer3Text;
+    private boolean seenBefore;
+
+    public Question(JSONObject questionJson) {
+
+    }
+
+    public Question(String questionText, String correctAnswerText,
+                    String wrongAnswer1Text, String wrongAnswer2Text,
+                    String wrongAnswer3Text, boolean seenBefore) {
+
+        this.questionText = questionText;
+        this.correctAnswerText = correctAnswerText;
+        this.wrongAnswer1Text = wrongAnswer1Text;
+        this.wrongAnswer2Text = wrongAnswer2Text;
+        this.wrongAnswer3Text = wrongAnswer3Text;
+        this.seenBefore = seenBefore;
+
+    }
+
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public String getCorrectAnswerText() {
+        return correctAnswerText;
+    }
+
+    public String getWrongAnswer1Text() {
+        return wrongAnswer1Text;
+    }
+
+    public String getWrongAnswer2Text() {
+        return wrongAnswer2Text;
+    }
+
+    public String getWrongAnswer3Text() {
+        return wrongAnswer3Text;
+    }
+
+    public boolean isSeenBefore() {
+        return seenBefore;
+    }
+
+}
