c95974c512641092deafd0a99c89805b76cb20a2
Nati <nati.tessema@gmail.com>
2014-06-09 02:45:31 -0700

    Hooked it up with the ListAdapter :)

--- hptrivia/app/src/main/java/com/titan/hptrivia/activity/ResultsActivity.java
+++ hptrivia/app/src/main/java/com/titan/hptrivia/activity/ResultsActivity.java
@@ -2,7 +2,6 @@ package com.titan.hptrivia.activity;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Parcel;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,11 +14,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.titan.hptrivia.R;
-import com.titan.hptrivia.model.Answer;
-import com.titan.hptrivia.model.Question;
 import com.titan.hptrivia.model.QuestionResponse;
 import com.titan.hptrivia.model.Quiz;
-import com.titan.hptrivia.model.QuizManager;
 import com.titan.hptrivia.model.QuizPersister;
 import com.titan.hptrivia.model.QuizResponse;
 
@@ -37,92 +33,19 @@ public class ResultsActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
 
-        QuizResponse datResponse = getIntent().getParcelableExtra("doz_quiz_results_doe");
-        Log.d(TAG, "response length = " + datResponse.size());
+
 
         // get questions from stored quiz
         Quiz quiz = QuizPersister.getInstance().getStoredQuiz();
-        arrayListQuestionResponses = QuizManager.getInstance().getQuizResponse().getAllQuestionResponses();
+        QuizResponse datResponse = getIntent().getParcelableExtra("doz_quiz_results_doe");
+        Log.d(TAG, "response length = " + datResponse.size());
 
-        Log.d(TAG, "ResultsActivity received a questionResponse with size " + arrayListQuestionResponses.size());
-
-//        for (int i = 0; i < quiz.size(); i++) {
-//            arrayListQuestionResponses.add(quiz.getQuestion(i));
-//        }
-
-        Answer ans = quiz.getQuestion(0).getCorrectAnswer();
-        Parcel parcelAns = Parcel.obtain();
-        ans.writeToParcel(parcelAns, 1);
-
-        parcelAns.setDataPosition(0);
-
-        Answer sameAnswer = new Answer(parcelAns);
-        Log.d(TAG, "Answer : " + ans.getText());
-        Log.d(TAG, "Parcelled Answer : " + sameAnswer.getText());
-
-        Question question = quiz.getQuestion(0);
-        Parcel pQ = Parcel.obtain();
-        question.writeToParcel(pQ, 1);
-
-        pQ.setDataPosition(0);
-
-        Question sameQuestion = new Question(pQ);
-        Log.d(TAG, "same question = " + sameQuestion.getQuestionText());
-        Log.d(TAG, "same question ans = " + sameQuestion.getCorrectAnswer().getText());
-
-        QuestionResponse qResp = new QuestionResponse(sameQuestion, sameQuestion.getCorrectAnswer());
-        Parcel pQR = Parcel.obtain();
-        qResp.writeToParcel(pQR, 1);
-
-        pQR.setDataPosition(0);
-
-        QuestionResponse sameQR = new QuestionResponse(pQR);
-
-        if (sameQR != null) {
-            if (sameQR.getQuestion() != null) {
-                Log.d(TAG, "! sameQuestionResponse q = " + sameQR.getQuestion().getQuestionText());
-            } else Log.e(TAG, "sameQR question == null");
-
-            if (sameQR.getAnswer() != null) {
-                Log.d(TAG, "same QuestiongResponse...a: " + sameQR.getAnswer().getText());
-            } else Log.e(TAG, "sameQR ans == null");
-        } else Log.e(TAG, "sameQR == null");
-
-
-        QuizResponse quizResp = new QuizResponse();
-        quizResp.addNewResponse(sameQR);
-        quizResp.addNewResponse(new QuestionResponse(quiz.getQuestion(4), quiz.getQuestion(4).getCorrectAnswer()));
-        quizResp.addNewResponse(new QuestionResponse(quiz.getQuestion(2), quiz.getQuestion(2).getCorrectAnswer()));
-
-        Log.d(TAG, "original quizResp size = " + quizResp.size());
-
-        for (QuestionResponse resp : quizResp.getAllQuestionResponses())
-            Log.d(TAG, "\tquestion : " + resp.getQuestion().getQuestionText());
-
-        Parcel quizR = Parcel.obtain();
-
-        quizResp.writeToParcel(quizR, 1);
-
-        quizR.setDataPosition(0);
-
-        QuizResponse sameQuizResp = new QuizResponse(quizR);
-
-        Log.d(TAG, "same QuizResp size = " + sameQuizResp.size());
-
-        for (QuestionResponse resp : sameQuizResp.getAllQuestionResponses()) {
-            if (resp != null) {
-                if (resp.getQuestion() != null) {
-                    Log.d(TAG, "\tquestion : " + resp.getQuestion().getQuestionText());
-                } else Log.d(TAG, "question = NULL");
-            }
-            else Log.d(TAG, "questionResponse : NULLL");
-        }
 
         // get views
         listViewResults = (ListView) findViewById(R.id.listViewResults);
 
         // set up list adapter
-        adapter = new ResultsAdapter(getApplicationContext(), arrayListQuestionResponses);
+        adapter = new ResultsAdapter(getApplicationContext(), datResponse.getAllQuestionResponses());
         listViewResults.setAdapter(adapter);
         listViewResults.setEmptyView(findViewById(android.R.id.empty));
 
@@ -166,12 +89,13 @@ public class ResultsActivity extends ActionBarActivity {
 
             QuestionResponse questionResponse = questionResponses.get(position);
 
-            TextView textViewQuestionNumber =  ((TextView) convertView.findViewById(R.id.textView_questionNumber));
-            TextView textViewQuestionText = ((TextView) convertView.findViewById(R.id.textView_question));
-            TextView textViewUserAnswer = ((TextView) convertView.findViewById(R.id.textView_yourAnsText));
-
             if (convertView == null) {
+
                 convertView = inflater.inflate(R.layout.cell_question_result, null);
+
+                TextView textViewQuestionNumber =  ((TextView) convertView.findViewById(R.id.textView_questionNumber));
+                TextView textViewQuestionText = ((TextView) convertView.findViewById(R.id.textView_question));
+                TextView textViewUserAnswer = ((TextView) convertView.findViewById(R.id.textView_yourAnsText));
 
                 // set the question number
                 textViewQuestionNumber.setText("Question " + (position + 1));
