482f8f1858316ab128d75a1053cf60e8925980d9
Nati <nati.tessema@gmail.com>
2014-06-18 01:50:25 -0700

    Got Google+ connection sort of working.

--- hptrivia/app/src/main/java/com/titan/hptrivia/activity/HomeActivity.java
+++ hptrivia/app/src/main/java/com/titan/hptrivia/activity/HomeActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +13,11 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
 import com.titan.hptrivia.R;
 import com.titan.hptrivia.model.NewQuizListener;
 import com.titan.hptrivia.model.Quiz;
@@ -22,7 +27,7 @@ import com.titan.hptrivia.util.CustomTextView;
 import com.titan.hptrivia.util.Utils;
 
 
-public class HomeActivity extends ActionBarActivity implements NewQuizListener {
+public class HomeActivity extends ActionBarActivity implements NewQuizListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = HomeActivity.class.getSimpleName();
 
@@ -32,11 +37,33 @@ public class HomeActivity extends ActionBarActivity implements NewQuizListener {
     // UI elements
     private Button buttonStartQuiz;
 
+    // Google+ auth
+    /* Request code used to invoke sign in user interactions. */
+    private static final int RC_SIGN_IN = 0;
+
+    /* Client used to interact with Google APIs. */
+    private GoogleApiClient mGoogleApiClient;
+
+    /* A flag indicating that a PendingIntent is in progress and prevents
+     * us from starting further intents.
+     */
+    private boolean mIntentInProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_home_new);
+
+        // Google+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+    //            .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
+
+        // set Google+ button click listener
 
         quizPersister = QuizPersister.getInstance();
         restClient = new RestClientImpl(getApplicationContext());
@@ -145,9 +172,19 @@ public class HomeActivity extends ActionBarActivity implements NewQuizListener {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
     protected void onStop() {
         super.onStop();
         quizPersister.removeNewQuizListener(this);
+
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
     }
 
     @Override
@@ -157,5 +194,53 @@ public class HomeActivity extends ActionBarActivity implements NewQuizListener {
         setProgressBarIndeterminateVisibility(false);
         Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(getApplicationContext(), "connected", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onConnected called");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e(TAG, "onConnectionSuspended called");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "onConnectionFailed");
+        Log.e(TAG, "error code " + connectionResult.getErrorCode());
+        Log.e(TAG, "connectionResult resolution: " + connectionResult.getResolution());
+        Log.e(TAG, "connectionResult.toString" + connectionResult.toString());
+
+        if (!mIntentInProgress && connectionResult.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+            //    startIntentSenderForResult(connectionResult.getResolution().getIntentSender(),
+            //            RC_SIGN_IN, null, 0, 0, 0);
+                connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+            } catch (android.content.IntentSender.SendIntentException e) {
+                // The intent was canceled before it was sent.  Return to the default
+                // state and attempt to connect to get an updated ConnectionResult.
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
+        Toast.makeText(getApplicationContext(), "connection failed", Toast.LENGTH_SHORT).show();
+    //    Log.e(TAG, "connection failed: (error code " + connectionResult.getErrorCode()
+    //            + "), \ntoString" + connectionResult.toString() + ",\nresolution.toString: " + connectionResult.getResolution().toString());
+    }
+
+    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            mIntentInProgress = false;
+            Log.d(TAG, "onActivityResult called");
+
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
+        }
     }
 }
