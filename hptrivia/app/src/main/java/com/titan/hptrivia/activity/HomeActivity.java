d22fbcf60db5b1d558af475200d016eeed0a7399
Nati <nati.tessema@gmail.com>
2014-06-20 16:39:08 -0700

    Using Android Studio created LoginActivity. About to create my own though..

--- hptrivia/app/src/main/java/com/titan/hptrivia/activity/HomeActivity.java
+++ hptrivia/app/src/main/java/com/titan/hptrivia/activity/HomeActivity.java
@@ -3,8 +3,10 @@ package com.titan.hptrivia.activity;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +30,7 @@ import com.titan.hptrivia.model.Quiz;
 import com.titan.hptrivia.model.QuizPersister;
 import com.titan.hptrivia.network.RestClientImpl;
 import com.titan.hptrivia.util.CustomTextView;
+import com.titan.hptrivia.util.Keys;
 import com.titan.hptrivia.util.Utils;
 
 
@@ -36,6 +40,8 @@ public class HomeActivity extends ActionBarActivity implements NewQuizListener, 
 
     private QuizPersister quizPersister;
     private RestClientImpl restClient;
+
+    private SharedPreferences prefs;
 
     // UI elements
     private Button buttonStartQuiz;
@@ -77,12 +83,17 @@ public class HomeActivity extends ActionBarActivity implements NewQuizListener, 
                 .addScope(Plus.SCOPE_PLUS_LOGIN)
                 .build();
 
-
         quizPersister = QuizPersister.getInstance();
         restClient = new RestClientImpl(getApplicationContext());
 
+        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        ((ImageView) findViewById(R.id.myGooglePic)).setBackgroundColor(getResources().getColor(R.color.red));
+
         inflateXML();
         setTitleBarFont();
+
+        // Make sure we're logged in
 
     }
 
@@ -122,6 +133,7 @@ public class HomeActivity extends ActionBarActivity implements NewQuizListener, 
                 }
             }
         });
+
     }
 
     private void setTitleBarFont() {
@@ -139,27 +151,6 @@ public class HomeActivity extends ActionBarActivity implements NewQuizListener, 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_home_settings:
-                Utils.makeShortToast(getApplicationContext(), "Home Settings");
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
         overridePendingTransition(0, 0);
@@ -167,28 +158,9 @@ public class HomeActivity extends ActionBarActivity implements NewQuizListener, 
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-//        mGoogleApiClient.connect();
-    }
-
-    @Override
     protected void onStop() {
         super.onStop();
         quizPersister.removeNewQuizListener(this);
-
-//        if (mGoogleApiClient.isConnected()) {
-//            mGoogleApiClient.disconnect();
-//        }
-    }
-
-    @Override
-    public void onNewQuizStored(Quiz quiz) {
-    //    buttonStartQuiz.setActivated(true);
-    //    buttonStartQuiz.setBackgroundColor(getResources().getColor(R.color.blue));
-        setProgressBarIndeterminateVisibility(false);
-        Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
-        startActivity(intent);
     }
 
     @Override
@@ -197,12 +169,15 @@ public class HomeActivity extends ActionBarActivity implements NewQuizListener, 
 
         if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
             Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+            Log.d(TAG, "id = " + currentPerson.getId());
             String personName = currentPerson.getDisplayName();
             Toast.makeText(getApplicationContext(), "connected as " + personName, Toast.LENGTH_SHORT).show();
-            Log.d(TAG, "personName = " + personName);
-            Log.d(TAG, "img url: " + currentPerson.getImage().getUrl());
-
             String personGooglePlusProfile = currentPerson.getUrl();
+
+            // store in SharedPrefs that user should auto-login next time
+            prefs.edit().putBoolean(Keys.PREFS.AUTO_LOGIN.name(), true).commit();
+            // TODO remove this from SharedPrefs when the user signs out
+
         } else Log.e(TAG, "current person == NULL");
     }
 
@@ -215,12 +190,7 @@ public class HomeActivity extends ActionBarActivity implements NewQuizListener, 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Log.e(TAG, "onConnectionFailed");
-        Log.e(TAG, "error code " + connectionResult.getErrorCode());
-        Log.e(TAG, "connectionResult resolution: " + connectionResult.getResolution());
-        Log.e(TAG, "connectionResult.toString" + connectionResult.toString());
-
         // handle different issues: http://goo.gl/WkezlF
-
         if (!mIntentInProgress) {
             // Store the ConnectionResult so that we can use it later when the user clicks
             // 'sign-in'.
@@ -233,21 +203,6 @@ public class HomeActivity extends ActionBarActivity implements NewQuizListener, 
             }
         }
 
-/*        if (!mIntentInProgress && connectionResult.hasResolution()) {
-            try {
-                mIntentInProgress = true;
-            //    startIntentSenderForResult(connectionResult.getResolution().getIntentSender(),
-            //            RC_SIGN_IN, null, 0, 0, 0);
-                connectionResult.startResolutionForResult(this, RC_SIGN_IN);
-            } catch (android.content.IntentSender.SendIntentException e) {
-                // The intent was canceled before it was sent.  Return to the default
-                // state and attempt to connect to get an updated ConnectionResult.
-                mIntentInProgress = false;
-                mGoogleApiClient.connect();
-            }
-        }*/
-    //    Log.e(TAG, "connection failed: (error code " + connectionResult.getErrorCode()
-    //            + "), \ntoString" + connectionResult.toString() + ",\nresolution.toString: " + connectionResult.getResolution().toString());
     }
 
     /* A helper method to resolve the current ConnectionResult error. */
@@ -278,5 +233,38 @@ public class HomeActivity extends ActionBarActivity implements NewQuizListener, 
                 mGoogleApiClient.connect();
             }
         }
+    }
+
+    @Override
+    public void onNewQuizStored(Quiz quiz) {
+        setProgressBarIndeterminateVisibility(false);
+        Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_home_settings:
+                Utils.makeShortToast(getApplicationContext(), "Home Settings");
+                return true;
+            case R.id.action_home_sign_out:
+                // remove "auto-login-ability"
+                prefs.edit().putBoolean(Keys.PREFS.AUTO_LOGIN.name(), false).commit();
+                Log.d(TAG, "Signing out. auto login = " + prefs.getBoolean(Keys.PREFS.AUTO_LOGIN.name(), false));
+                Intent intent = new Intent(this, LoginActivity.class);
+                this.startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
